7e92f9b87bff421f9199da8761340c0907f5f674
Emmanuel Luciano <emma.luciano11@gmail.com>
2019-04-22 15:21:27 -0400

    Upgrades storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,24 @@
 const path = require("path");
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
 
-module.exports = (baseConfig, env, config) => {
+const styledComponentsTransformer = createStyledComponentsTransformer();
+
+module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.tsx?$/,
     include: path.resolve(__dirname, "../src"),
     use: [
-      require.resolve("babel-loader"),
-      require.resolve("react-docgen-typescript-loader"),
-    ],
+    {
+      loader: require.resolve("awesome-typescript-loader"),
+      options: {
+        getCustomTransformers: () => ({
+          before: [styledComponentsTransformer]
+        })
+      },
+    },
+    {
+      loader: require.resolve("react-docgen-typescript-loader")
+    }],
   });
 
   config.resolve.extensions.push(".ts", ".tsx");
